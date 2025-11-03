271e6e849d1557f80723e425b62c52fe144bb1ad
Nardone-Giampietro <nashj6@live.it>
2025-11-03 15:18:13 +0100

    first commit

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq" // Import the pq driver for PostgreSQL
 )
 
 // Declare a string containing the application version number
@@ -16,6 +20,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 // Struct to hold the dependencies for the HTTP handlers, helpers and middleware.
@@ -32,7 +39,18 @@ func main() {
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.Parse()
 
+	// Set the database connection string based on the environment
+	cfg.db.dsn = "postgres://greenlight:pa55word@localhost:5432/greenlight?sslmode=disable"
+
 	logger := log.New(os.Stdout, "", log.LstdFlags|log.Lshortfile)
+
+	// Set the database connection string based on the environment
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		config: cfg,
@@ -54,5 +72,22 @@ func main() {
 	if err := svr.ListenAndServe(); err != nil {
 		logger.Fatal(err)
 	}
+}
 
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// create context with a timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// ping the database to check if it's reachable
+	if err := db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,8 +1,9 @@
 package data
 
 import (
-	"greenlight.nardone.xyz/internal/validator"
 	"time"
+
+	"greenlight.nardone.xyz/internal/validator"
 )
 
 type Movie struct {
@@ -10,20 +11,19 @@ type Movie struct {
 	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitempty"`
-	//Call the Runtime type in order to MarshalJSON to work
+	// Call the Runtime type in order to MarshalJSON to work
 	Runtime Runtime  `json:"runtime,omitempty"`
 	Genres  []string `json:"genres,omitempty"`
 	Version int32    `json:"version"`
 }
 
 func ValidateMovie(v *validator.Validator, inputMovie *Movie) {
-
 	v.Check(inputMovie.Title != "", "title", "must be provided")
 	v.Check(len(inputMovie.Title) <= 500, "title", "must not be more than 500 bytes")
 
 	v.Check(inputMovie.Year != 0, "year", "must be provided")
 	v.Check(inputMovie.Year >= 1888, "year", "must be greater than or equal to 1888")
-	v.Check(inputMovie.Year <= int32(time.Now().Year()), "year", "must be less than current year")
+	v.Check(inputMovie.Year <= int32(time.Now().Year()), "year", "must be less th current year")
 
 	v.Check(inputMovie.Runtime != 0, "runtime", "must be provided")
 	v.Check(inputMovie.Runtime > 0, "runtime", "must be greater than or equal to 0")
